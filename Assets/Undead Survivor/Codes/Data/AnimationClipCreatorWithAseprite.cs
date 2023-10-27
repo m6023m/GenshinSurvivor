8d82e77e7d9715b057ac5bded4e92b560ebca0cd
m6023m <m6023m@naver.com>
2023-10-27 17:07:12 +0900

    Add Animation clips

--- Assets/Undead Survivor/Codes/Data/AnimationClipCreatorWithAseprite.cs
+++ Assets/Undead Survivor/Codes/Data/AnimationClipCreatorWithAseprite.cs
@@ -13,17 +13,27 @@ public class AnimationClipCreatorWithAseprite : ScriptableObject
     public GameObject asepriteObject;
     [BoxGroup("Animation Settings"), Tooltip("Folder path to save generated animation clip"), FolderPath]
     public string saveFolderPath = "Assets";
+    public List<AnimationEventer> animationStartEvents = new List<AnimationEventer>();
+    public List<AnimationEventer> animationEndEvents = new List<AnimationEventer>();
+
+    [Serializable]
+    public class AnimationEventer
+    {
+        public string name;
+        public string eventName;
+    }
 
     [Button]
     public void CreateAnimationClips()
     {
+        Debug.Log("Start CreateAnimationClips");
         Animator myAnimator = asepriteObject.GetComponent<Animator>();
         AnimationClip[] clips = GetAnimationClips(myAnimator);
         foreach (AnimationClip clip in clips)
         {
             CreateAnimationClipsFromKeyframes(clip);
         }
-
+        Debug.Log("End CreateAnimationClips");
     }
 
     public void CreateAnimationClipsFromKeyframes(AnimationClip clip)
@@ -41,11 +51,39 @@ public class AnimationClipCreatorWithAseprite : ScriptableObject
 
             AnimationUtility.SetObjectReferenceCurve(newClip, editorCurve, keyframes);
 
+
+            AddEvent(newClip, animationStartEvents, 0);
+            AddEvent(newClip, animationEndEvents, newClip.length);
+
+
+
             newClips.Add(newClip);
 
             AssetDatabase.CreateAsset(newClip, $"{saveFolderPath}/{name}.anim");
+
         }
 
+    }
+    public void AddEvent(AnimationClip newClip, List<AnimationEventer> animationEventers, float time)
+    {
+        List<AnimationEvent> eventsToAdd = new List<AnimationEvent>();
+        foreach (AnimationEventer animationEventer in animationEventers)
+        {
+            if (newClip.name.Contains(animationEventer.name))
+            {
+                AnimationEvent animationEvent = new AnimationEvent
+                {
+                    time = time,
+                    functionName = animationEventer.eventName
+                };
+                eventsToAdd.Add(animationEvent);
+            }
+        }
+        AnimationEvent[] currentEvents = AnimationUtility.GetAnimationEvents(newClip);
+        List<AnimationEvent> allEvents = new List<AnimationEvent>(currentEvents);
+        allEvents.AddRange(eventsToAdd);
+
+        AnimationUtility.SetAnimationEvents(newClip, allEvents.ToArray());
     }
     public AnimationClip[] GetAnimationClips(Animator animator)
     {
